1f8023f7b78bd756b232626d39fe48584ca18060
Jeff Zhang <945jeff@gmail.com>
2020-08-24 16:29:17 -0400

    make it better

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
 const app=express()
 const items=require('./routes/api/items')
+const path=require('path')
 
 app.use(bodyParser.json())
 app.use('/api/items', items)
+
+if(process.env.NODE_ENV==='production') {
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 //mongo db
 const db=require('./config/keys').mongoURI 
