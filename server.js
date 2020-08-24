126faa3494d07966678e0c6a449993d50f83beda
Jeff Zhang <945jeff@gmail.com>
2020-08-24 18:20:11 -0400

    make it better

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose
     .then(() => console.log('mongoDB connected...'))
     .catch(err => console.log(err))
 
-const port_number = server.listen(process.env.PORT || 5000);
+const port_number = process.env.PORT || 5000
 app.listen(port_number);
