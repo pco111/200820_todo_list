5d13d4924a83cd9e196a08ec6d36efaa4c495615
Jeff Zhang <945jeff@gmail.com>
2020-08-24 19:57:07 -0400

    second commit

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const items=require('./routes/api/items')
 app.use(bodyParser.json())
 app.use('/api/items', items)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
+
 //mongo db
 const db=require('./config/keys').mongoURI 
 mongoose
     .connect(db,{useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => console.log('mongoDB connected...'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err)) 
 
 app.listen(process.env.PORT || 5000)
